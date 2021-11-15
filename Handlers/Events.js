5d58a0bc3fd4c2c1a3604944624a6dead839aa1e
KinchiFR <sounafr@protonmail.com>
2021-11-15 13:54:34 +0100

    useless await removed

--- Handlers/Events.js
+++ Handlers/Events.js
@@ -12,7 +12,7 @@ module.exports = async (client) => {
 
         if (!Events.includes(event.name) || !event.name) {
             const L = file.split("/");
-            await Table.addRow(`${event.name || "MISSING"}`, `⛔ Event name is either invalid or missing: ${L[6] + `/` + L[7]}`);
+            Table.addRow(`${event.name || "MISSING"}`, `⛔ Event name is either invalid or missing: ${L[6] + `/` + L[7]}`);
             return;
         }
 
@@ -22,7 +22,7 @@ module.exports = async (client) => {
             client.on(event.name, (...args) => event.execute(client, ...args));
         };
 
-        await Table.addRow(event.name, "✔ SUCCESSFUL");
+        Table.addRow(event.name, "✔ SUCCESSFUL");
     });
 
     console.log(Table.toString());
