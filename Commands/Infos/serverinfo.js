f69f5cb4d162b6230fdcbbbf3c5bdd5a58e12f59
KinchiFR <sounafr@protonmail.com>
2021-11-15 13:53:30 +0100

    users presence update

--- Commands/Infos/serverinfo.js
+++ Commands/Infos/serverinfo.js
@@ -1,6 +1,4 @@
 const { CommandInteraction, Client, MessageEmbed } = require('discord.js');
-const { checkDays } = require('../../utils/function');
-const moment = require('moment');
 
 module.exports = {
     name: "serverinfo",
@@ -12,16 +10,19 @@ module.exports = {
     async execute(interaction, client) {
         //variables
         const guild = interaction.guild;
+        await guild.members.fetch();
+        const guildOwner = await guild.fetchOwner();
         const channelCache = guild.channels.cache;
         const presenceCache = guild.presences.cache;
-        const creationDate = moment.utc(guild.createdAt).format("DD/MM/YYYY");
 
         // presence calculation
-        const online = `🟢 Online : ${presenceCache.filter((presence) => presence.status === "online").size}\n`;
-        const idle = `🌙 Idle : ${presenceCache.filter((presence) => presence.status === "idle").size}\n`;
-        const dnd = `⛔ Do not disturb : ${presenceCache.filter((presence) => presence.status === "dnd").size}\n`;
-        const offline = `⭕ offline : ${presenceCache.filter((presence) => presence.status === "offline").size}\n`
-        let presenceString = online + idle + dnd + offline;
+        const totalMembers = guild.memberCount;
+        const online = presenceCache.filter((presence) => presence.status === "online").size;
+        const idle = presenceCache.filter((presence) => presence.status === "idle").size;
+        const dnd = presenceCache.filter((presence) => presence.status === "dnd").size;
+        const offline = totalMembers - (online + idle + dnd);
+        let presenceString = `🟢 En ligne : ${online}\n🌙 Absents : ${idle}\n⛔ Ne pas déranger : ${dnd}\n⭕ Hors ligne : ${offline}`
+
 
         // verification levels for "guild.verificationLevel" field
         const verifLevels = {
@@ -32,19 +33,22 @@ module.exports = {
             "VERY_HIGH": "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"
         };
 
-        // get the list of members
-        await guild.members.fetch();
-        const guildOwner = await guild.fetchOwner();
+        const premiumTier = {
+            "NONE": "0",
+            "TIER_1": "1",
+            "TIER_2": "2",
+            "TIER_3": "3"
+        };
 
         const embed = new MessageEmbed()
             .setColor("GREEN")
             .setThumbnail(guild.iconURL({ dynamic: true }))
             .addField("📝 Nom", guild.name)
             .addField("👑 Preaupryaitères", `${guildOwner}`)
-            .addField("🚀 Boosts", `Niveau : ${guild.premiumTier} | ${guild.premiumSubscriptionCount} boosts`, true)
+            .addField("🚀 Boosts", `**Niveau** : ${premiumTier[guild.premiumTier]} | ${guild.premiumSubscriptionCount} boosts`, true)
             .addField("✅ Niveau de vérification", verifLevels[guild.verificationLevel], true)
-            .addField("🕒 Date de création", `${creationDate}\n${checkDays(guild.createdAt)}`, true)
-            .addField("👥 Status de membre", presenceString)
+            .addField("🕒 Date de création", `<t:${parseInt(guild.createdTimestamp / 1000)}:R>`, true)
+            .addField("👥 Status de membre", `${presenceString}`)
             .addField("🤖", `**Bots** : ${guild.members.cache.filter((member) => member.user.bot === true).size.toString()}`, true)
             .addField("📜", `**Rôles** : ${guild.roles.cache.filter((role) => role.name != "@everyone").size.toString()}`, true)
             .addField("☺", `**Nombre d'emojis** : ${guild.emojis.cache.size.toString()}`, true)
