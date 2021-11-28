4cae391c5b2954016249aecbcd59ceea281dc275
KinchiFR <sounafr@protonmail.com>
2021-11-28 20:03:05 +0100

    description changed

--- Events/Member/guildMemberRemove.js
+++ Events/Member/guildMemberRemove.js
@@ -8,7 +8,7 @@ module.exports = {
     execute(client, member) {
         const logEmbed = new MessageEmbed()
             .setColor("RED")
-            .setDescription(`${member} just left the server`)
+            .setDescription(`${member}/${member.user.tag} just left the server`)
             .setTimestamp();
 
         member.guild.channels.cache.get("899621885346734110").send({ embeds: [logEmbed] });
