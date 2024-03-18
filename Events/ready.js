2306f6af3f24a1cb6cb8acbd00ac86af17fcdf2a
SounaVR <59102661+SounaVR@users.noreply.github.com>
2024-03-18 08:22:06 +0100

    monitoring support through uptime kuma

--- Events/ready.js
+++ Events/ready.js
@@ -1,6 +1,7 @@
 const { MONGO_URL } = process.env;
 const { Events, ActivityType } = require('discord.js');
 const mongoose  = require('mongoose');
+const axios = require('axios');
 
 module.exports = {
     name: Events.ClientReady,
@@ -12,6 +13,14 @@ module.exports = {
         }).catch((err) => {
             console.log(err);
         });
+
+        // Monitoring stuff | Uptime Kuma
+        setInterval(() => {
+            axios({
+                method: 'get',
+                url: `http://192.168.0.16:3001/api/push/gzlHoDwFAt?status=up&msg=OK&ping=${client.ws.ping}`
+            });
+        }, 30000);
         
         client.user.setActivity('SounaTV', { type: ActivityType.Streaming, url: "https://www.twitch.tv/sounatv" });
     
